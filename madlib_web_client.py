65d4d539e1eec1e70c5900e5146a3cb2fa3fb408
Mike Putnam <mike@theputnams.net>
2016-02-01 17:13:49 -0600

    Add minimal postgres + heroku code.

--- madlib_web_client.py
+++ madlib_web_client.py
@@ -1,5 +1,39 @@
 import os
 from flask import Flask
+import psycopg2
+from urllib.parse import urlparse
+
+url = urlparse(os.environ["DATABASE_URL"])
+
+# Connect to a database
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Create a table
+cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+
+# Insert test data
+cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
+
+# Query the database and obtain data as Python objects
+cur.execute("SELECT * FROM test;")
+print(cur.fetchone())
+
+# Make the changes to the database persistent
+conn.commit()
+
+# Close the cursor and the connection to the database
+cur.close()
+conn.close()
+
 
 app = Flask(__name__)
 
