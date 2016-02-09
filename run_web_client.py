769348413bce3ddc0e5214bb170ad8c5e4a78946
Mike Putnam <mike@theputnams.net>
2016-02-09 09:15:22 -0600

    Add os import to fix port from env

--- run_web_client.py
+++ run_web_client.py
@@ -1,3 +1,6 @@
 # Run a test server.
 from app import app
-app.run(host='0.0.0.0', port=5000, debug=False)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
