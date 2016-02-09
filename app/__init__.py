cb9c5c3ae7660cf15c0d30e18a827f419f262d72
Mike Putnam <mike@theputnams.net>
2016-02-09 09:06:53 -0600

    Etc

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
-db.create_all()
+
 
 from app.models import Madlib
+db.create_all()
+db.session.commit()
+
 m = Madlib("A cheese madlib", "Here is where the body of this madlib would be")
 
 db.session.add(m)
